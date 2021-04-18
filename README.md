d71c2863e4bca42c29db7287bca19ae49ada904d
Thais Cavalcanti <morena@felipes-macbook-pro-2.home>
2021-04-18 20:44:37 +0200

    Initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Pairing interview exercise - Movies API
---
+++ src/test/kotlin/com/scentmate/movies/MoviesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.scentmate.movies
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
